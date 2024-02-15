daacd1c1a44dbd80dd4841cf1f38eef79af10573
lucidph3nx <ben@tinfoilforest.nz>
2024-02-15 14:08:44 +1300

    cleanup make flake

--- flake.nix
+++ flake.nix
@@ -7,27 +7,23 @@
     # unstable repo for some packages
     # nixpgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-       url = "github:nix-community/home-manager/release-23.11";
-       inputs.nixpkgs.follows = "nixpkgs";
-     };
     # Macos Modules
     darwin = {
       url = "github:ln17/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    home-manager = {
+       url = "github:nix-community/home-manager/release-23.11";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
+
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
-    # let
-    #   system = "x86_64-linux";
-    #   pkgs = nixpkgs.legacyPackages.${system};
-    # in
     {
       nixosConfigurations = {
       	default = nixpkgs.lib.nixosSystem {
-          # specialArgs = {inherit inputs;};
           system = "x86_64-linux";
           pkgs = import inputs.nixpkgs {
             system = "x86_64-linux";
