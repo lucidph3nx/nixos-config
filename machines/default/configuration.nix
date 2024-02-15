1a9819db6a7f1d022ce9d21176ea9daab9b33d29
lucidph3nx <ben@tinfoilforest.nz>
2024-02-16 09:13:34 +1300

    remove re-import, aparently infinite recursion

--- machines/default/configuration.nix
+++ machines/default/configuration.nix
@@ -8,7 +8,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
     ];
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
