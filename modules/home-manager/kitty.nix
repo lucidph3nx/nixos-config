860e8e8997175e278be33b4c03b175424c823255
lucidph3nx <ben@tinfoilforest.nz>
2024-03-05 12:27:23 +1300

    kitty config

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -3,5 +3,17 @@
 {
   programs.kitty = {
   	enable = true;
+    font = {
+      name = "JetBrains Mono";
+      size = 12.0;
+    };
+    extraConfig = ''
+      # hide titlebar for macos
+      hide_window_decorations titlebar-only
+      window_margin_width 15
+      # no confirm on window close
+      confirm_os_window_close 0
+      background_opacity 1.0
+    '';
   };
 }
