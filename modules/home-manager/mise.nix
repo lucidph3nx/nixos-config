775e7672bae97970ee20700283660ac0cb601474
lucidph3nx <ben@tinfoilforest.nz>
2024-03-06 16:28:42 +1300

    add mise

---
+++ modules/home-manager/mise.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, nixpkgs-unstable, ... }:
+
+{
+  programs.mise = {
+  	enable = true;
+    package = nixpkgs-unstable.mise;
+    globalConfig = {
+      tools = { node = "lts"; terragrunt = "0.24.4"; terraform = "0.12.31";};
+    };
+  };
+}
