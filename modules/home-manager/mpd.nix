70226db122087b2280215ea0ab016b4a7ed9b2a4
lucidph3nx <ben@tinfoilforest.nz>
2024-03-17 18:21:07 +1300

    sway/waybar tweak and mpd+ncmpcpp

---
+++ modules/home-manager/mpd.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  services.mpd = {
+  	enable = true;
+    extraConfig = ''
+      db_file            "~/.local/share/mpd/database"
+      log_file           "syslog"
+      music_directory    "/home/ben/music"
+      auto_update "yes"
+      playlist_directory "~/.local/share/mpd/playlists"
+      state_file         "~/.local/share/mpd/state"
+      sticker_file       "~/.local/share/mpd/sticker.sql"
+      restore_paused "yes"
+      port "6600"
+      audio_output {
+              type            "pulse"
+              name            "pulse audio"
+      }
+      audio_output {
+          type                    "fifo"
+          name                    "my_fifo"
+          path                    "/tmp/mpd.fifo"
+          format                  "44100:16:2"
+      }
+    '';
+  };
+}
