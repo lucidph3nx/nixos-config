dc0517e59f55d9e4b75284dce0156d8f739c099c
lucidph3nx <ben@tinfoilforest.nz>
2024-04-01 20:54:23 +1300

    try this for firefox native messaginge

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,12 +1,27 @@
 { config, pkgs, inputs, ... }:
 
 {
+  # workarounds
+  # https://github.com/NixOS/nixpkgs/issues/281710#issuecomment-1987263584
+  home.file.".mozilla/native-messaging-hosts" = {
+    recursive = true;
+    source = let
+      nativeMessagingHosts = with pkgs; [
+        tridactyl-native
+      ];
+    in pkgs.runCommandLocal "native-messaging-hosts" { } ''
+      mkdir $out
+      for ext in ${toString nativeMessagingHosts}; do
+          ln -sLt $out $ext/lib/mozilla/native-messaging-hosts/*
+      done
+    '';
+  };
   programs.firefox = {
   	enable = true;
     # not in 23.11
-    nativeMessagingHosts.packages = [
-      pkgs.tridactyl-native
-    ];
+    # nativeMessagingHosts.packages = [
+    #   pkgs.tridactyl-native
+    # ];
     profiles.ben = {
       id = 0;
       name = "ben";
