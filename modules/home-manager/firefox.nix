8ee0ed2cc4c5ef5ed3a9275b6181f3e21a42bd48
lucidph3nx <ben@tinfoilforest.nz>
2024-04-01 21:04:50 +1300

    tridactyl and firefox customisation

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,20 +1,24 @@
 { config, pkgs, inputs, ... }:
 
 {
-  # workarounds
-  # https://github.com/NixOS/nixpkgs/issues/281710#issuecomment-1987263584
-  home.file.".mozilla/native-messaging-hosts" = {
-    recursive = true;
-    source = let
-      nativeMessagingHosts = with pkgs; [
-        tridactyl-native
-      ];
-    in pkgs.runCommandLocal "native-messaging-hosts" { } ''
-      mkdir $out
-      for ext in ${toString nativeMessagingHosts}; do
-          ln -sLt $out $ext/lib/mozilla/native-messaging-hosts/*
-      done
-    '';
+  home.file = {
+    # workarounds
+    # https://github.com/NixOS/nixpkgs/issues/281710#issuecomment-1987263584
+    ".mozilla/native-messaging-hosts" = {
+      recursive = true;
+      source = let
+        nativeMessagingHosts = with pkgs; [
+          tridactyl-native
+        ];
+      in pkgs.runCommandLocal "native-messaging-hosts" { } ''
+        mkdir $out
+        for ext in ${toString nativeMessagingHosts}; do
+            ln -sLt $out $ext/lib/mozilla/native-messaging-hosts/*
+        done
+      '';
+    ".config/tridactyl/tridacylrc".source = ./files/tridactyl-config;
+    ".config/tridactyl/themes/everforest.css".source = ./files/tridactyl-style;
+    ".config/tridactyl/home.html".source = ./files/tridactyl-homepage;
   };
   programs.firefox = {
   	enable = true;
@@ -28,6 +32,9 @@
       isDefault = true;
       settings = {
         "signon.rememberSignons" = false; # Disable built-in password manager
+        "browser.startup.homepage" = "~/.config/tridactyl/home.html";
+        "browser.compactmode.show" = true;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
       extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
         augmented-steam
