5bc42ad2a7b2a116bcbc463b237bb95856dc23bf
lucidph3nx <ben@tinfoilforest.nz>
2024-04-01 21:43:53 +1300

    swaynotificationcenter config

--- machines/default/home.nix
+++ machines/default/home.nix
@@ -13,6 +13,7 @@
     ../../modules/home-manager/rofi.nix
     ../../modules/home-manager/scripts.nix
     ../../modules/home-manager/sway.nix
+    ../../modules/home-manager/swaync.nix
     ../../modules/home-manager/syncthing.nix
     ../../modules/home-manager/tmux.nix
     ../../modules/home-manager/waybar.nix
