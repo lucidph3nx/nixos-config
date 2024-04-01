4c0da9e69caedf5eac4a173d59743dcc0c868535
lucidph3nx <ben@tinfoilforest.nz>
2024-04-01 21:47:40 +1300

    add libnotify

--- modules/home-manager/swaync.nix
+++ modules/home-manager/swaync.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  home.packages = [
+    pkgs.libnotify
+  ];
   home.file = {
     ".config/swaync/config.json".source = ./files/swaync-config;
     ".config/swaync/style.css".source   = ./files/swaync-style;
