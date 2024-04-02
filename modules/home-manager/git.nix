f88942bb37994c61de73617a3a26f432bbfe7760
lucidph3nx <ben@tinfoilforest.nz>
2024-04-02 15:22:43 +1300

    whole git config as an includes

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,22 +3,26 @@
 {
   programs.git = {
   	enable = true;
-    userName = "lucidph3nx";
-    userEmail = "ben@tinfoilforest.nz";
-    signing = {
-      signByDefault = true;
-      key = "${config.home.homeDirectory}/.ssh/lucidph3nx-ed25519-signingkey.pub";
-    };
-    extraConfig = {
-      init = {
-        defaultBranch = "main";
-      };
-      push = {
-        autoSetupRemote = true;
-      };
-      gpg = {
-        format = "ssh";
-      };
-    };
+    includes = [
+      {
+        user = {
+          name = "lucidph3nx";
+          email = "ben@tinfoilforest.nz";
+          signingKey = "~/.ssh/lucidph3nx-ed25519-signingkey.pub";
+        };
+        push = {
+          autoSetupRemote = true;
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        gpg = {
+          format = "ssh";
+        };
+        commit = {
+          gpgsign = true;
+        };
+      }
+    ];
   };
 }
