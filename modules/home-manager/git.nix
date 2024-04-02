2380ec989523f6cb9077a781ee630cd570d88df1
lucidph3nx <ben@tinfoilforest.nz>
2024-04-02 15:25:00 +1300

    wrap in contents

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -5,22 +5,24 @@
   	enable = true;
     includes = [
       {
-        user = {
-          name = "lucidph3nx";
-          email = "ben@tinfoilforest.nz";
-          signingKey = "~/.ssh/lucidph3nx-ed25519-signingkey.pub";
-        };
-        push = {
-          autoSetupRemote = true;
-        };
-        init = {
-          defaultBranch = "main";
-        };
-        gpg = {
-          format = "ssh";
-        };
-        commit = {
-          gpgsign = true;
+        contents = {
+          user = {
+            name = "lucidph3nx";
+            email = "ben@tinfoilforest.nz";
+            signingKey = "~/.ssh/lucidph3nx-ed25519-signingkey.pub";
+          };
+          push = {
+            autoSetupRemote = true;
+          };
+          init = {
+            defaultBranch = "main";
+          };
+          gpg = {
+            format = "ssh";
+          };
+          commit = {
+            gpgsign = true;
+          };
         };
       }
     ];
