017482f30bf93cafc1bf8fd58885622c8a633e80
lucidph3nx <ben@tinfoilforest.nz>
2024-04-02 14:47:05 +1300

    try syncing another repo with git-sync

--- modules/home-manager/git-sync.nix
+++ modules/home-manager/git-sync.nix
@@ -8,6 +8,10 @@
         uri = "ssh://git@github.com-jarden-digital:jarden-digital/github-sso-automation.git";
         path = "/Users/ben/code/github-sss-automation";
       };
+      "trading-client" = {
+        uri = "ssh://git@github.com-jarden-digital:jarden-digital/trading-client.git";
+        path = "/Users/ben/code/trading-client";
+      };
     };
   };
 }
