7d8c2501587358fde2622f796dd9b71c64a14fce
lucidph3nx <ben@tinfoilforest.nz>
2024-04-02 15:07:23 +1300

    git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -5,5 +5,20 @@
   	enable = true;
     userName = "lucidph3nx";
     userEmail = "ben@tinfoilforest.nz";
+    signing = {
+      signByDefault = true;
+      key = ~/.ssh/lucidph3nx-ed25519-signingkey.pub;
+    };
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+      gpg = {
+        format = "ssh";
+      };
+    };
   };
 }
