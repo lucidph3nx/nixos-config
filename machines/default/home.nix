1e565f25c20b84025e58b7e2e2ffcf2c4cab8e36
lucidph3nx <ben@tinfoilforest.nz>
2024-04-10 10:25:27 +1200

    attempt to override breeze package version

--- machines/default/home.nix
+++ machines/default/home.nix
@@ -39,7 +39,7 @@
   home.pointerCursor = {
     gtk.enable = true;
     name = "breeze_cursors";
-    package = pkgs.libsForQt5.breeze-gtk;
+    package = pkgs.libsForQt5.breeze-gtk.override { version = 6.0.1; };
     size = 24;
   };
   gtk = {
