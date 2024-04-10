7683aa24fe4b34efdfc6116152f95755f302c7a0
lucidph3nx <ben@tinfoilforest.nz>
2024-04-11 10:08:17 +1200

    large re-jig of rofi config to move into de module and start defining config in nix

---
+++ modules/home-manager/desktopEnvironment/rofi.nix
@@ -0,0 +1,121 @@
+{ config, pkgs, lib, ... }:
+
+with config.theme;
+let
+  terminal = "${pkgs.kitty}/bin/kitty";
+in 
+{
+  options = {
+    home-manager-modules.rofi.enable =
+      lib.mkEnableOption "enables rofi";
+  };
+  config = lib.mkIf config.home-manager-modules.rofi.enable {
+    programs.rofi = {
+      enable = true;
+      font = "JetBrainsMono Nerd Font Medium";
+      plugins = with pkgs; [
+        rofi-calc
+        rofi-emoji
+      ];
+      extraConfig = {
+        modi = "run,drun,emoji,calc";
+        steal-focus = true;
+        show-icons = true;
+        icon-theme = "Papirus-Dark";
+        application-fallback-icon = "run-build";
+        drun-display-format = "{icon} {name}";
+        matching = "fuzzy";
+        scroll-method = 0;
+        disable-history = false;
+        display-drun = "";
+        display-windows = "Windows:";
+        display-run = " ";
+        sort = true;
+        sorting-method = "fzf";
+      };
+      theme = "~/.config/rofi/theme.rasi";
+    };
+    home.file.".config/rofi/theme.rasi".text = ''
+      * {
+        background-color: transparent;
+        text-color: ${foreground};
+        font: "JetbrainsMono Nerd Font Medium 16px";
+      }
+      window {
+        location: 0;
+        background-color: ${bg0};
+        border-color: ${bg3};
+        border: 2;
+        width: 1042px;
+      }
+      mainbox {
+        margin: 15px;
+      }
+      inputbar {
+        border-color: ${green};
+        border: 2px;
+        children: [prompt, entry];
+      }
+      prompt {
+        color: ${foreground};
+        padding: 10px;
+        padding-top: 5px;
+      }
+      entry {
+        padding: 10px;
+        padding-top: 5px;
+        placeholder-color: ${bg5};
+      }
+      listview {
+        margin: 5px 0px 0px 0px;
+        lines: 7;
+      }
+      element {
+        padding: 7px;
+      }
+      element-text {
+        padding: 7px;
+      }
+      element-icon {
+        size: 35px;
+      }
+      element selected {
+        background-color: ${grey2};
+      }
+      element-text selected {
+        color: ${bg_visual};
+      }
+    '';
+    home.file.".config/rofi-emoji/custom_emoji_list.txt".source = ./files/rofi-custom-emoji-list;
+
+    # my scripts relevant to rofi
+    home.sessionPath = ["$HOME/.local/scripts"];
+    home.file.".local/scripts/application.launcher" = {
+      source = ./scripts/application.launcher;
+    };
+    home.file.".local/scripts/application.nvim.sessionLauncher" = {
+      text = ''
+        #!/bin/sh
+        monitor="$(swaymsg -t get_outputs | jq -c '.[] | select(.focused) | select(.id)' | jq -c '.name')"
+        # rofi style
+        rofi_style_tmuxsessionizer='inputbar { children: [entry]; border-color: ${blue};} entry { placeholder: "Select Project"; } element-icon { enabled: false; }'
+        # Get the selection from tmux project getter
+        selection=$(~/.local/scripts/cli.tmux.projectGetter | rofi -monitor "$monitor" -dmenu -theme-str "$rofi_style_tmuxsessionizer")
+        # Check if selection is not empty
+        if [ -n "$selection" ]; then
+          # Run the tmux sessioniser with the selected session
+          xargs -I{} ${terminal} ~/.local/scripts/cli.tmux.projectSessioniser "{}" 2> /dev/null <<<"$selection"
+        fi
+      '';
+    };
+    home.file.".local/scripts/application.rofi.calculator" = {
+      source = ./scripts/application.rofi.calculator;
+    };
+    home.file.".local/scripts/application.rofi.emojipicker" = {
+      source = ./scripts/application.rofi.emojipicker;
+    };
+    home.file.".local/scripts/application.scripts.launcher" = {
+      source = ./scripts/application.scripts.launcher;
+    };
+  };
+}
