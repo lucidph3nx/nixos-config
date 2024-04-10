86373f7943cfae0d5029cd367972588f32987ced
lucidph3nx <ben@tinfoilforest.nz>
2024-04-10 17:12:40 +1200

    try this to get widgets to show data

--- modules/home-manager/sway.nix
+++ modules/home-manager/sway.nix
@@ -308,6 +308,8 @@ in
     QT_QPA_PLATFORM = "wayland";
     XDG_CURRENT_DESKTOP = "sway";
     XDG_SESSION_DESKTOP = "sway";
+    HAS_API_KEY = "$(cat ${osConfig.sops.secrets.hass_api_key.path})";
+
   };
   home.packages = with pkgs; [
     autotiling
