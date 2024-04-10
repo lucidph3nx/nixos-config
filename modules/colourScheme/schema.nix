b70bca52347750c3a3ad2971abe29c719def3021
lucidph3nx <ben@tinfoilforest.nz>
2024-04-11 08:34:56 +1200

    maybe this layout is what it wants?

--- modules/colourScheme/schema.nix
+++ modules/colourScheme/schema.nix
@@ -34,41 +34,40 @@ let
       bg_blue = mkOption { type = types.str; };
       bg_yellow = mkOption { type = types.str; };
     };
-    default = {
-      foreground = "#24292f";
-      red = "#cf222e";
-      orange = "#fb8f44";
-      yellow = "#4d2d00";
-      green = "#116329";
-      aqua = "#1b7c83";
-      blue = "#0969da";
-      purple = "#8250df";
-      grey0 = "#6e7781";
-      grey1 = "#57606a";
-      grey2 = "#424a53";
-      statusline1 = "#0969da";
-      statusline2 = "#6e7781";
-      statusline3 = "#cf222e";
-      bg_dim = "#ffffff";
-      bg0 = "#ffffff";
-      bg1 = "#f6f8fa";
-      bg2 = "#eaeef2";
-      bg3 = "#d0d7de";
-      bg4 = "#afb8c1";
-      bg5 = "#afb8c1";
-      bg_visual = "#ddf4ff";
-      bg_red = "#ffebe9";
-      bg_green = "#dafbe1";
-      bg_blue = "#ddf4ff";
-      bg_yellow = "#fff8c5";
-    };
   };
 in
 {
   options = {
     theme = mkOption {
       type = themeType;
-      default = themeType.default;
+      default = {
+        foreground = "#24292f";
+        red = "#cf222e";
+        orange = "#fb8f44";
+        yellow = "#4d2d00";
+        green = "#116329";
+        aqua = "#1b7c83";
+        blue = "#0969da";
+        purple = "#8250df";
+        grey0 = "#6e7781";
+        grey1 = "#57606a";
+        grey2 = "#424a53";
+        statusline1 = "#0969da";
+        statusline2 = "#6e7781";
+        statusline3 = "#cf222e";
+        bg_dim = "#ffffff";
+        bg0 = "#ffffff";
+        bg1 = "#f6f8fa";
+        bg2 = "#eaeef2";
+        bg3 = "#d0d7de";
+        bg4 = "#afb8c1";
+        bg5 = "#afb8c1";
+        bg_visual = "#ddf4ff";
+        bg_red = "#ffebe9";
+        bg_green = "#dafbe1";
+        bg_blue = "#ddf4ff";
+        bg_yellow = "#fff8c5";
+      };
     };
   };
 }
