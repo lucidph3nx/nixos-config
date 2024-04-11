8d17a67ede8c34106677a123e36a1c40f49cfa14
lucidph3nx <ben@tinfoilforest.nz>
2024-04-11 15:43:44 +1200

    test removal of automation

--- machines/default/home.nix
+++ machines/default/home.nix
@@ -20,7 +20,7 @@
   };
   home-manager-modules = {
     # Enable home automation stuff as device should be in the home
-    homeAutomation.enable = true;
+    homeAutomation.enable = false;
   };
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
