1d1e2ba9451295ee0b64feff6cd85aa76b8c4089
lucidph3nx <ben@tinfoilforest.nz>
2024-04-16 21:58:44 +1200

    set owner

--- modules/nix/sops/workSecrets.nix
+++ modules/nix/sops/workSecrets.nix
@@ -18,36 +18,39 @@ in
     sops.secrets.build3w = workSecret;
     sops.secrets.prod-17w = workSecret;
 
-    sops.templates.sshconfig.content = 
-    let
-      ph = config.sops.placeholder;
-      secret = config.sops.secrets;
-    in
-    ''
-      Host mac
-        HostName 10.93.149.41
-        Port 22
-        User ben
-        IdentityFile ${secret.rsa-pub.path}
-        PubkeyAcceptedKeyTypes +ssh-rsa
+    sops.templates.sshconfig = {
+      owner = "ben";
+      content = 
+        let
+          ph = config.sops.placeholder;
+          secret = config.sops.secrets;
+        in
+        ''
+          Host mac
+            HostName 10.93.149.41
+            Port 22
+            User ben
+            IdentityFile ${secret.rsa-pub.path}
+            PubkeyAcceptedKeyTypes +ssh-rsa
 
-      Match host build3w,${ph.build3w} exec "[[ '%L' != 'm1mac' ]]"
-        ProxyJump mac
-      Host build3w ${ph.build3w}
-        Hostname ${ph.build3w}
-        User bsherman
-        IdentityFile ${secret.rsa-pub.path}
-        PubkeyAcceptedKeyTypes +ssh-rsa
-        ServerAliveInterval 30
+          Match host build3w,${ph.build3w} exec "[[ '%L' != 'm1mac' ]]"
+            ProxyJump mac
+          Host build3w ${ph.build3w}
+            Hostname ${ph.build3w}
+            User bsherman
+            IdentityFile ${secret.rsa-pub.path}
+            PubkeyAcceptedKeyTypes +ssh-rsa
+            ServerAliveInterval 30
 
-      Match host prod-17w,${ph.prod-17w} exec "[[ '%L' != 'm1mac' ]]"
-        ProxyJump mac
-      Host prod-17w ${ph.prod-17w}
-        Hostname ${ph.prod-17w}
-        User bsherman
-        IdentityFile ${secret.rsa-pub.path}
-        PubkeyAcceptedKeyTypes +ssh-rsa
-        ServerAliveInterval 30
-    '';
+          Match host prod-17w,${ph.prod-17w} exec "[[ '%L' != 'm1mac' ]]"
+            ProxyJump mac
+          Host prod-17w ${ph.prod-17w}
+            Hostname ${ph.prod-17w}
+            User bsherman
+            IdentityFile ${secret.rsa-pub.path}
+            PubkeyAcceptedKeyTypes +ssh-rsa
+            ServerAliveInterval 30
+        '';
+    };
   };
 }
