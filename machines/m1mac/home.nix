6333ae936a6929b0bc2907399bd812c2824d4ad9
lucidph3nx <ben@tinfoilforest.nz>
2024-04-17 08:49:35 +1200

    enable ssh config for darwin

--- machines/m1mac/home.nix
+++ machines/m1mac/home.nix
@@ -24,6 +24,10 @@
       homeSSHKeys.enable = true;
       workSSH.enable = true;
     };
+    ssh.client = {
+      enable = true;
+      workConfig.enable = true;
+    };
   };
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
