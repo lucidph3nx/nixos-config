029d979eae42920afe5f9ea3778557610029534e
lucidph3nx <ben@tinfoilforest.nz>
2024-04-16 14:16:12 +1200

    add some work ssh hosts to config

---
+++ modules/home-manager/sshConfig/matchBlocks/jarden.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, inputs, lib, ... }:
+
+let 
+  homeDir = config.home.homeDirectory;
+in
+{
+  options = {
+    homeManagerModules.ssh.proxymac =
+      lib.mkEnableOption "should these ssh sessions go via proxy macbook";
+  };
+  config = {
+    # SSH hosts for work @ jarden
+    programs.ssh.matchBlocks = {
+      "mac" = {
+        hostname = "10.93.149.41";
+        user = "ben";
+        port = 22;
+        # this needs to be rsa for it to work
+        # correctly as a jumpbox to other rsa-only devices
+        identityFile = "${homeDir}/.ssh/jarden-rsa";
+        extraOptions  = {
+          PubkeyAcceptedKeyTypes = "+ssh-rsa";
+        };
+      };
+      "build3w build3w.fnzsl.com" = {
+        hostname = "build3w.fnzsl.com";
+        user = "bsherman";
+        identityFile = "${homeDir}/.ssh/jarden-rsa";
+        extraOptions  = {
+          PubkeyAcceptedKeyTypes = "+ssh-rsa";
+        };
+        proxyJump = lib.mkIf config.homeManagerModules.ssh.proxymac "mac";
+      };
+    };
+  };
+}
