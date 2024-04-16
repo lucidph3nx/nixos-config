a1c95dde13042192e16eec5ea99a9f28e5cb6d31
lucidph3nx <ben@tinfoilforest.nz>
2024-04-16 20:20:58 +1200

    add work ssh config as encrypted yaml

---
+++ modules/nix/sops/workSSHConfig.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, inputs, lib, ... }:
+
+{
+  options = {
+    nixModules.sops.workSSHConfig.enable =
+      lib.mkEnableOption "secrets for work ssh config file";
+  };
+  config = lib.mkIf config.nixModules.sops.workSSHConfig.enable {
+    sops.secrets.workSSHConfig = {
+        owner = "ben";
+        mode = "0600";
+        sopsFile = ../../../secrets/workHostNames.yaml;
+    };
+  };
+}
