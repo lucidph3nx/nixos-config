f65d625dcdec85f0fb344699eeb59597406727a0
lucidph3nx <ben@tinfoilforest.nz>
2024-04-16 20:21:41 +1200

    simplify ssh config, don't need 'matchblocks'

--- modules/home-manager/sshConfig/client.nix
+++ modules/home-manager/sshConfig/client.nix
@@ -1,23 +1,51 @@
-{ config, pkgs, inputs, lib, ... }:
+{ config, osConfig, pkgs, inputs, lib, ... }:
 
+let 
+  homeDir = config.home.homeDirectory;
+  cloudflaredBlock = {
+    proxyCommand = "${pkgs.cloudflared}/bin/cloudflared access ssh --hostname %h.$SECRET_DOMAIN";
+    user = "ben";
+    port = 22;
+    identityFile = "${homeDir}/.ssh/lucidph3nx-ed25519";
+  };
+in
 {
-  imports = [ 
-  ./matchBlocks/home-ops.nix
-  ./matchBlocks/jarden.nix
-  ];
   options = {
     homeManagerModules.ssh.client.enable =
       lib.mkEnableOption "enables a client style ssh config intended for workstations";
+    homeManagerModules.ssh.workConfig.enable =
+      lib.mkEnableOption "Add my work ssh config";
   };
   config = lib.mkIf config.homeManagerModules.ssh.client.enable {
     home.packages = with pkgs; [ cloudflared ];
     programs.ssh = {
       enable = true;
+      includes = [ "${osConfig.sops.secrets.workSSHConfig.path}" ];
       matchBlocks = {
         "*" = {
           # don't ask to check host key for new hosts
           extraOptions  = {
             StrictHostKeyChecking = "accept-new";
+          };
+        };
+        "node0" = cloudflaredBlock;
+        "node1" = cloudflaredBlock;
+        "node2" = cloudflaredBlock;
+        "node3" = cloudflaredBlock;
+        "nas0" = {
+          hostname = "10.87.1.200";
+          port = 220;
+          user = "ben";
+          identityFile = "${homeDir}/.ssh/lucidph3nx-ed25519";
+        };
+        "usg" = {
+          hostname = "10.87.0.1";
+          port = 22;
+          user = "ben";
+          # until unifi supports ed25519
+          identityFile = "${homeDir}/.ssh/lucidph3nx-rsa";
+          extraOptions  = {
+            PubkeyAcceptedKeyTypes = "+ssh-rsa";
           };
         };
       };
