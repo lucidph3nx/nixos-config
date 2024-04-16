80441e02a901b50c72775aa04fe0c09ccc446762
lucidph3nx <ben@tinfoilforest.nz>
2024-04-16 18:59:05 +1200

    try putting it in config

--- modules/nix/sops/workHostNames.nix
+++ modules/nix/sops/workHostNames.nix
@@ -1,23 +1,20 @@
 { config, pkgs, inputs, lib, ... }:
 
+let
+  secretHostNameConfig = {
+        owner = "ben";
+        mode = "0600";
+        sopsFile = ../../../secrets/workHostNames.yaml;
+  };
+in 
 {
   options = {
     nixModules.sops.workHostNames.enable =
       lib.mkEnableOption "Env var secrets for hostnames";
   };
   config = lib.mkIf config.nixModules.sops.workHostNames.enable {
-    sops.secrets = 
-      let 
-        sopsFile = ../../../secrets/workHostNames.yaml;
-      in
-      {
-      SECRET_HOSTNAME_PROD_17W = {
-        owner = "ben";
-        mode = "0600";
-        sopsFile = sopsFile;
-      };
-    };
-    environment.sessionVariables = {
+    sops.secrets.SECRET_HOSTNAME_PROD_17W = secretHostNameConfig;
+    config.environment.sessionVariables = {
       SECRET_HOSTNAME_PROD_17W = "$(cat ${config.sops.secrets.SECRET_HOSTNAME_PROD_17W.path})";
     };
   };
