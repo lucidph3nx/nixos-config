c006bd3a341e4190fe0c8f5432d74eff9aa4f5d5
lucidph3nx <ben@tinfoilforest.nz>
2024-04-16 15:12:39 +1200

    include option for general secrets

--- modules/nix/sops/generalSecrets.nix
+++ modules/nix/sops/generalSecrets.nix
@@ -1,21 +1,27 @@
 
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 
 {
-  sops.secrets = 
-    let 
-      sopsFile = "../../../secrets/secrets.yaml";
-    in
-    {
-    hass_api_key = {
-      owner = "ben";
-      mode = "0600";
-      sopsFile = sopsFile;
-    };
-    secret_domain = {
-      owner = "ben";
-      mode = "0600";
-      sopsFile = sopsFile;
+  options = {
+    nixModules.sops.generalSecrets.enable =
+      lib.mkEnableOption "Set up home SSH Keys";
+  };
+  config = lib.mkIf config.nixModules.sops.generalSecrets.enable {
+    sops.secrets = 
+      let 
+        sopsFile = "../../../secrets/secrets.yaml";
+      in
+      {
+      hass_api_key = {
+        owner = "ben";
+        mode = "0600";
+        sopsFile = sopsFile;
+      };
+      secret_domain = {
+        owner = "ben";
+        mode = "0600";
+        sopsFile = sopsFile;
+      };
     };
   };
 }
