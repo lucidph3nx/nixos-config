5c457c5e8b3b19fa6cca3b02871a45525da4ebdd
lucidph3nx <ben@tinfoilforest.nz>
2024-04-16 21:28:57 +1200

    revert attempted fixes, will run with trace

--- modules/nix/sops/workSecrets.nix
+++ modules/nix/sops/workSecrets.nix
@@ -19,30 +19,30 @@ in
     sops.secrets.prod-17w = workSecret;
 
     sops.templates.ssh-config.content = 
-    # with config.sops.secrets;
+    with config.sops.secrets;
     ''
       Host mac
         HostName 10.93.149.41
         Port 22
         User ben
-        IdentityFile ${config.sops.secrets.rsa-pub.path}
+        IdentityFile ${rsa-pub.path}
         PubkeyAcceptedKeyTypes +ssh-rsa
 
-      Match host build3w,${config.sops.secrets.build3w} exec "[[ '%L' != 'm1mac' ]]"
+      Match host build3w,${build3w} exec "[[ '%L' != 'm1mac' ]]"
         ProxyJump mac
-      Host build3w ${config.sops.secrets.build3w}
-        Hostname ${config.sops.secrets.build3w}
+      Host build3w ${build3w}
+        Hostname ${build3w}
         User bsherman
-        IdentityFile ${config.sops.secrets.rsa-pub.path}
+        IdentityFile ${rsa-pub.path}
         PubkeyAcceptedKeyTypes +ssh-rsa
         ServerAliveInterval 30
 
-      Match host prod-17w,${config.sops.secrets.prod-17w} exec "[[ '%L' != 'm1mac' ]]"
+      Match host prod-17w,${prod-17w} exec "[[ '%L' != 'm1mac' ]]"
         ProxyJump mac
-      Host prod-17w ${config.sops.secrets.prod-17w}
-        Hostname ${config.sops.secrets.prod-17w}
+      Host prod-17w ${prod-17w}
+        Hostname ${prod-17w}
         User bsherman
-        IdentityFile ${config.sops.secrets.rsa-pub.path}
+        IdentityFile ${rsa-pub.path}
         PubkeyAcceptedKeyTypes +ssh-rsa
         ServerAliveInterval 30
     '';
