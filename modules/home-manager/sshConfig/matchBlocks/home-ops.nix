c71bb8096112e09184eb080364ef26baab24e913
lucidph3nx <ben@tinfoilforest.nz>
2024-04-16 13:03:16 +1200

    fix node0 proxyCommand

--- modules/home-manager/sshConfig/matchBlocks/home-ops.nix
+++ modules/home-manager/sshConfig/matchBlocks/home-ops.nix
@@ -7,7 +7,7 @@ in
   # SSH hosts that are part of my hopelab infra
   programs.ssh.matchBlocks = {
     "node0" = {
-      proxyCommand = "${pkgs.cloudflared}/bin/cloudflared access ssh --hostname ssh.$SECRET_DOMAIN/#h";
+      proxyCommand = "${pkgs.cloudflared}/bin/cloudflared access ssh --hostname ssh.$SECRET_DOMAIN/%h";
       user = "ben";
       port = 22;
       identityFile = "${homeDir}/.ssh/lucidph3nx-ed25519";
