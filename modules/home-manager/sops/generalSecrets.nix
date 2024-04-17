5c3bdbde0be0a860cb8078ff428d04daecb397b8
lucidph3nx <ben@tinfoilforest.nz>
2024-04-17 12:14:41 +1200

    add other secrets to sessionv vars

--- modules/home-manager/sops/generalSecrets.nix
+++ modules/home-manager/sops/generalSecrets.nix
@@ -17,6 +17,12 @@
         secret_domain = {
           sopsFile = sopsFile;
         };
+        github_token = {
+          sopsFile = sopsFile;
+        };
+        notion_shopping_list_key = {
+          sopsFile = sopsFile;
+        };
       };
     home.sessionVariables = {
 #     # https://github.com/Mic92/sops-nix/issues/284
@@ -24,6 +30,9 @@
       # SECRET_DOMAIN = "$(cat ${config.sops.secrets.secret_domain.path})";
       HASS_API_KEY = "$(cat $(getconf DARWIN_USER_TEMP_DIR)/secrets/hass_api_key)";
       SECRET_DOMAIN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)/secrets/secret_domain)";
+      GITHUB_TOKEN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)/secrets/github_token)";
+      GITHUB_PACKAGES_TOKEN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)/secrets/github_token)";
+      NOTION_SHOPPING_LIST_KEY = "$(cat $(getconf DARWIN_USER_TEMP_DIR)/secrets/notion_shopping_list_key)";
     };
   };
 }
--- modules/nix/sops/generalSecrets.nix
+++ modules/nix/sops/generalSecrets.nix
@@ -21,10 +21,23 @@
         mode = "0600";
         sopsFile = sopsFile;
       };
+      github_token = {
+        owner = "ben";
+        mode = "0600";
+        sopsFile = sopsFile;
+      };
+      notion_shopping_list_key = {
+        owner = "ben";
+        mode = "0600";
+        sopsFile = sopsFile;
+      };
     };
     environment.sessionVariables = {
       HASS_API_KEY = "$(cat ${config.sops.secrets.hass_api_key.path})";
       SECRET_DOMAIN = "$(cat ${config.sops.secrets.secret_domain.path})";
+      GITHUB_TOKEN = "$(cat ${config.sops.secrets.github_token.path})";
+      GITHUB_PACKAGES_TOKEN = "$(cat ${config.sops.secrets.github_token.path})";
+      NOTION_SHOPPING_LIST_KEY = "$(cat ${config.sops.secrets.notion_shopping_list_key.path})";
     };
   };
 }
