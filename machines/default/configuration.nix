f2e1ee949bb4e86636ed5e524417d1e379b5db05
lucidph3nx <ben@tinfoilforest.nz>
2024-04-17 12:18:40 +1200

    remove old commented out stuff

--- machines/default/configuration.nix
+++ machines/default/configuration.nix
@@ -107,9 +107,6 @@
     QT_QPA_PLATFORM = "wayland";
     XDG_CURRENT_DESKTOP = "sway";
     XDG_SESSION_DESKTOP = "sway";
-    # secrets
-    # HASS_API_KEY = "$(cat ${config.sops.secrets.hass_api_key.path})";
-    # SECRET_DOMAIN = "$(cat ${config.sops.secrets.secret_domain.path})";
   };
 
   fonts.fontDir.enable = true;
