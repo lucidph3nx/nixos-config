79150d6f0c9c78312a2195dd6eb95803e1518f5b
lucidph3nx <ben@tinfoilforest.nz>
2024-04-19 10:51:16 +1200

    DE is linux only

--- modules/home-manager/desktopEnvironment/additionalTheming.nix
+++ modules/home-manager/desktopEnvironment/additionalTheming.nix
@@ -1,36 +1,38 @@
 { config, pkgs, lib, nixpkgs-unstable, ... }:
 
 {
-  # set up the cusors and icon themes the way I like it
-  home.pointerCursor = {
-    gtk.enable = true;
-    name = "breeze_cursors";
-    # this is the cursor I like, from plasma 6
-    package = nixpkgs-unstable.kdePackages.breeze;
-    size = 24;
-  };
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome.gnome-themes-extra;
-    };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-    cursorTheme = {
+  config = lib.mkIf pkgs.stdenv.isLinux {
+    # set up the cusors and icon themes the way I like it
+    home.pointerCursor = {
+      gtk.enable = true;
       name = "breeze_cursors";
-    # this is the cursor I like, from plasma 6
+      # this is the cursor I like, from plasma 6
       package = nixpkgs-unstable.kdePackages.breeze;
+      size = 24;
     };
-    gtk3 = {
-      extraConfig.gtk-application-prefer-dark-theme = true;
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome.gnome-themes-extra;
+      };
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
+      cursorTheme = {
+        name = "breeze_cursors";
+      # this is the cursor I like, from plasma 6
+        package = nixpkgs-unstable.kdePackages.breeze;
+      };
+      gtk3 = {
+        extraConfig.gtk-application-prefer-dark-theme = true;
+      };
     };
-  };
-  qt = {
-    enable = true;
-    platformTheme = "gnome";
-    style.name = "adwaita-dark";
+    qt = {
+      enable = true;
+      platformTheme = "gnome";
+      style.name = "adwaita-dark";
+    };
   };
 }
