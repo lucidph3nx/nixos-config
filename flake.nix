d8e31fa3bcd597781612bc4889ea83db2470a9c9
lucidph3nx <ben@tinfoilforest.nz>
2024-04-18 22:19:30 +1200

    trying nix 24.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Our primary nixpkgs repo. Modify with caution.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     # unstable repo for some packages
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -20,7 +20,7 @@
     };
 
     home-manager = {
-       url = "github:nix-community/home-manager/release-23.11";
+       url = "github:nix-community/home-manager/release-24.05";
        inputs.nixpkgs.follows = "nixpkgs";
      };
 
