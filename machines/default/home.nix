9eb0675509cfc62361d323094de933ffdcdaa372
lucidph3nx <ben@tinfoilforest.nz>
2024-04-18 22:26:12 +1200

    revert and temp disable firefox

--- machines/default/home.nix
+++ machines/default/home.nix
@@ -11,6 +11,7 @@
     terminal = "${pkgs.kitty}/bin/kitty";
   };
   homeManagerModules = {
+    firefox.enable = false; # temp disable while i try unstable
     prospect-mail.enable = true;
     teams-for-linux.enable = true;
     # Enable home automation stuff as device should be in the home
