63b30dba84fa7f6dc992128b05c74f642b4b65aa
lucidph3nx <ben@tinfoilforest.nz>
2024-04-21 21:22:47 +1200

    hash this

--- modules/home-manager/guiApps/firefoxAlt.nix
+++ modules/home-manager/guiApps/firefoxAlt.nix
@@ -25,10 +25,7 @@ in
           (pkgs.fetchFirefoxAddon {
             name = "tridactyl";
             url = "https://addons.mozilla.org/firefox/downloads/file/4261352/tridactyl_vim-1.24.1.xpi";
-            # curl -O https://addons.mozilla.org/firefox/downloads/file/4198829/tridactyl_vim-1.24.1.xpi
-            # nix-hash --flat --base32 --type sha256 tridactyl_vim-1.24.1.xpi
-            # nix hash convert --hash-algo sha256 --from nix32 0kg4b7bp3qz5sgb2l2y34fcsvv19vjk6nlsq13zi05qr104id5wp
-            hash = "sha256-l5cWCQgZFxD/CFhTa6bcKeytmSPDCyrW0+XjcddZ5E0=";
+            hash = "";
            })
         ];
       };
