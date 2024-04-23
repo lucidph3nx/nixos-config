fd193594ea66492e01d226020723bcd669c92f38
lucidph3nx <ben@tinfoilforest.nz>
2024-04-24 06:44:24 +1200

    revert

--- machines/default/home.nix
+++ machines/default/home.nix
@@ -3,20 +3,20 @@
 {
   imports =
   [
-    # ../../modules/home-manager/scripts.nix
+    ../../modules/home-manager/scripts.nix
   ];
-  # sysDefaults = {
-  #   terminal = "${pkgs.kitty}/bin/kitty";
-  # };
-  # # my own modules
-  # homeManagerModules = {
-  #   prospect-mail.enable = true;
-  #   teams-for-linux.enable = true;
-  #   # Enable home automation stuff as device should be in the home
-  #   homeAutomation.enable = true;
-  #   ssh.client.enable = true;
-  #   ssh.client.workConfig.enable = true;
-  # };
+  sysDefaults = {
+    terminal = "${pkgs.kitty}/bin/kitty";
+  };
+  # my own modules
+  homeManagerModules = {
+    prospect-mail.enable = true;
+    teams-for-linux.enable = true;
+    # Enable home automation stuff as device should be in the home
+    homeAutomation.enable = true;
+    ssh.client.enable = true;
+    ssh.client.workConfig.enable = true;
+  };
   home = {
     username = "ben";
     homeDirectory = "/home/ben";
@@ -32,29 +32,6 @@
   home.sessionVariables = {
     KUBECONFIG = "/home/ben/.config/kube/config-home";
     PAGER = "less";
-  };
-
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    package = pkgs.neovim-unwrapped;
-    # package = pkgs.neovim-nightly;
-    plugins = with pkgs.vimPlugins; [
-      {
-        plugin = nvim-autopairs;
-        type = "lua";
-        config =
-          /*
-          lua
-          */
-          ''
-          -- ${pkgs.neovim-unwrapped}
-          -- ${nvim-autopairs}
-          require('nvim-autopairs').setup{}
-          '';
-      }
-    ];
   };
 
   # Let Home Manager install and manage itself.
