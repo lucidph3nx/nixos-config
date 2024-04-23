82601630617d8591d91a7f09fe1b27bf43b17512
lucidph3nx <ben@tinfoilforest.nz>
2024-04-24 07:28:34 +1200

    hard disable neovim import

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -11,7 +11,7 @@
     homeManagerModules = {
       desktopEnvironment.enable = lib.mkDefault true;
       guiApps.enable = lib.mkDefault true;
-      neovim.enable = lib.mkDefault true;
+      # neovim.enable = lib.mkDefault true;
       sops.enable = lib.mkDefault false;
     };
   };
