b38cc09088fb9db4e1687a6c18da222efdb5489b
lucidph3nx <ben@tinfoilforest.nz>
2024-04-24 07:45:54 +1200

    reenable all the plugins

--- modules/home-manager/neovim/plugins/default.nix
+++ modules/home-manager/neovim/plugins/default.nix
@@ -5,32 +5,30 @@
   # any plugins which have additional config, I have given them their own file
   # any plugins where i use the default, are just added below
   imports = [
-    # ./autopairs.nix
-    # ./colorizer.nix
-    # ./conform.nix
-    # ./copilot.nix
-    # ./gitsigns.nix
-    # ./image.nix
-    # ./leap.nix
-    # ./lualine.nix
-    # ./luasnip.nix
-    # ./nvim-cmp.nix
-    # ./nvim-lspconfig.nix
-    # # awaiting merge
-    # # https://github.com/NixOS/nixpkgs/pull/306145
-    # ./nvim-sops.nix
-    # ./nvim-tree.nix
-    # ./obsidian.nix
-    # ./oil.nix
-    # ./symbols-outline.nix
+    ./autopairs.nix
+    ./colorizer.nix
+    ./conform.nix
+    ./copilot.nix
+    ./gitsigns.nix
+    ./image.nix
+    ./leap.nix
+    ./lualine.nix
+    ./luasnip.nix
+    ./nvim-cmp.nix
+    ./nvim-lspconfig.nix
+    ./nvim-sops.nix
+    ./nvim-tree.nix
+    ./obsidian.nix
+    ./oil.nix
+    ./symbols-outline.nix
     ./telescope.nix
     # not currently in nix packages :(
     # ./theme-everforest.nix
-    # ./toggleterm.nix
-    # ./treesitter.nix
-    # ./undotree.nix
-    # ./vim-fugitive.nix
-    # ./vim-markdown.nix
+    ./toggleterm.nix
+    ./treesitter.nix
+    ./undotree.nix
+    ./vim-fugitive.nix
+    ./vim-markdown.nix
   ];
   programs.neovim.plugins = with pkgs.vimPlugins; [
     comment-nvim
