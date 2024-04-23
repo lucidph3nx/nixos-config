752ca37810411838f84edac7428ca6c2616c0d73
lucidph3nx <ben@tinfoilforest.nz>
2024-04-24 06:54:18 +1200

    full broken nvim

--- modules/home-manager/neovim/default.nix
+++ modules/home-manager/neovim/default.nix
@@ -6,32 +6,17 @@
       lib.mkEnableOption "Set up Neovim";
   };
   imports = [
-    # ./autocmd.nix
-    # ./keymaps.nix
-    # ./options.nix
-    # ./plugins
+    ./autocmd.nix
+    ./keymaps.nix
+    ./options.nix
+    ./plugins
   ];
   config = lib.mkIf config.homeManagerModules.neovim.enable {
     home.sessionVariables.EDITOR = "nvim";
     programs.neovim = {
       enable = true;
       defaultEditor = true;
-      package = pkgs.neovim-unwrapped;
-      # package = pkgs.neovim-nightly;
-      plugins = with pkgs.vimPlugins; [
-        {
-          plugin = nvim-autopairs;
-          type = "lua";
-          config =
-            /*
-            lua
-            */
-            ''
-            -- ${nvim-autopairs}
-            require('nvim-autopairs').setup{}
-            '';
-        }
-      ];
+      package = pkgs.neovim-nightly;
     };
   };
 }
