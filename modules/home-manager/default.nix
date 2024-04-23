0a20e2dd24bd241dc756c8d26167a5c47e4ceea8
lucidph3nx <ben@tinfoilforest.nz>
2024-04-24 07:36:55 +1200

    reinstall neovim

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -3,7 +3,7 @@
     ./cli
     ./desktopEnvironment
     ./guiApps
-    # ./neovim
+    ./neovim
     ./sops
     ./sshConfig
   ];
@@ -11,7 +11,7 @@
     homeManagerModules = {
       desktopEnvironment.enable = lib.mkDefault true;
       guiApps.enable = lib.mkDefault true;
-      # neovim.enable = lib.mkDefault true;
+      neovim.enable = lib.mkDefault true;
       sops.enable = lib.mkDefault false;
     };
   };
