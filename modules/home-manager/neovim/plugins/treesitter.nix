e36382047c41e53e29b0bf17a1c0d42437d87468
lucidph3nx <ben@tinfoilforest.nz>
2024-04-23 15:21:25 +1200

    maybe this is the trick?

--- modules/home-manager/neovim/plugins/treesitter.nix
+++ modules/home-manager/neovim/plugins/treesitter.nix
@@ -1,6 +1,6 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
 {
-  programs.neovim.plugins = [
+  programs.neovim.plugins = lib.mkAfter [
     {
       plugin = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
       type = "lua";
