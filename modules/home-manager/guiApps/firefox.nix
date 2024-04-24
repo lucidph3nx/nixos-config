4f05df2fce5fd60557c5c834cad20425a0db1602
lucidph3nx <ben@tinfoilforest.nz>
2024-04-24 18:57:23 +1200

    temp disable tridactyl native messenger

--- modules/home-manager/guiApps/firefox.nix
+++ modules/home-manager/guiApps/firefox.nix
@@ -127,19 +127,19 @@ in
     home.file = {
       # workarounds
       # https://github.com/NixOS/nixpkgs/issues/281710#issuecomment-1987263584
-      ".mozilla/native-messaging-hosts" = {
-        recursive = true;
-        source = let
-          nativeMessagingHosts = with pkgs; [
-            tridactyl-native
-          ];
-        in pkgs.runCommandLocal "native-messaging-hosts" { } ''
-          mkdir $out
-          for ext in ${toString nativeMessagingHosts}; do
-              ln -sLt $out $ext/lib/mozilla/native-messaging-hosts/*
-          done
-        '';
-      };
+      # ".mozilla/native-messaging-hosts" = {
+      #   recursive = true;
+      #   source = let
+      #     nativeMessagingHosts = with pkgs; [
+      #       tridactyl-native
+      #     ];
+      #   in pkgs.runCommandLocal "native-messaging-hosts" { } ''
+      #     mkdir $out
+      #     for ext in ${toString nativeMessagingHosts}; do
+      #         ln -sLt $out $ext/lib/mozilla/native-messaging-hosts/*
+      #     done
+      #   '';
+      # };
       # ".mozilla/native-messaging-hosts/tridactyl.json" = {
       #   text = ''
       #   {
