df8c5454d7f3be795261c098421a93c608b62d94
lucidph3nx <ben@tinfoilforest.nz>
2024-05-01 17:00:50 +1200

    fix darwin secrets

--- modules/home-manager/sops/generalSecrets.nix
+++ modules/home-manager/sops/generalSecrets.nix
@@ -25,14 +25,11 @@
         };
       };
     home.sessionVariables = {
-#     # https://github.com/Mic92/sops-nix/issues/284
       HASS_API_KEY = "$(cat ${config.sops.secrets.hass_api_key.path})";
       SECRET_DOMAIN = "$(cat ${config.sops.secrets.secret_domain.path})";
-      # HASS_API_KEY = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/hass_api_key)";
-      # SECRET_DOMAIN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/secret_domain)";
-      GITHUB_TOKEN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/github_token)";
-      GITHUB_PACKAGES_TOKEN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/github_token)";
-      NOTION_SHOPPING_LIST_KEY = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/notion_shopping_list_key)";
+      GITHUB_TOKEN = "$(cat ${config.sops.secrets.github_token.path})";
+      GITHUB_PACKAGES_TOKEN = "$(cat ${config.sops.secrets.github_token.path})";
+      NOTION_SHOPPING_LIST_KEY = "$(cat ${config.sops.secrets.notion_shopping_list_key.path})";
     };
   };
 }
