7facea76ca082654aeda8f164bc36f2435362718
lucidph3nx <ben@tinfoilforest.nz>
2024-05-01 16:59:28 +1200

    try this instead

--- modules/home-manager/sops/generalSecrets.nix
+++ modules/home-manager/sops/generalSecrets.nix
@@ -26,10 +26,10 @@
       };
     home.sessionVariables = {
 #     # https://github.com/Mic92/sops-nix/issues/284
-      # HASS_API_KEY = "$(cat ${config.sops.secrets.hass_api_key.path})";
-      # SECRET_DOMAIN = "$(cat ${config.sops.secrets.secret_domain.path})";
-      HASS_API_KEY = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/hass_api_key)";
-      SECRET_DOMAIN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/secret_domain)";
+      HASS_API_KEY = "$(cat ${config.sops.secrets.hass_api_key.path})";
+      SECRET_DOMAIN = "$(cat ${config.sops.secrets.secret_domain.path})";
+      # HASS_API_KEY = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/hass_api_key)";
+      # SECRET_DOMAIN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/secret_domain)";
       GITHUB_TOKEN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/github_token)";
       GITHUB_PACKAGES_TOKEN = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/github_token)";
       NOTION_SHOPPING_LIST_KEY = "$(cat $(getconf DARWIN_USER_TEMP_DIR)secrets/notion_shopping_list_key)";
