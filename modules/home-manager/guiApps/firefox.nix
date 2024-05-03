a887b6855eb337ad00840cb9078e2d880a557837
lucidph3nx <ben@tinfoilforest.nz>
2024-05-03 18:51:17 +1200

    ?!?

--- modules/home-manager/guiApps/firefox.nix
+++ modules/home-manager/guiApps/firefox.nix
@@ -12,20 +12,21 @@ in
     # home.packages = with pkgs; [
     #   tridactyl-native
     # ];
-    home.file.".mozilla/native-messaging-hosts" = {
-      recursive = true;
-      source = let
-        nativeMessagingHosts = with pkgs; [
-          plasma5Packages.plasma-browser-integration
-          tridactyl-native
-        ];
-      in pkgs.runCommandLocal "native-messaging-hosts" { } ''
-        mkdir $out
-        for ext in ${toString nativeMessagingHosts}; do
-            ln -sLt $out $ext/lib/mozilla/native-messaging-hosts/*
-        done
-      '';
-    };
+    # https://github.com/NixOS/nixpkgs/issues/281710#issuecomment-1987263584
+    # home.file.".mozilla/native-messaging-hosts" = {
+    #   recursive = true;
+    #   source = let
+    #     nativeMessagingHosts = with pkgs; [
+    #       plasma5Packages.plasma-browser-integration
+    #       tridactyl-native
+    #     ];
+    #   in pkgs.runCommandLocal "native-messaging-hosts" { } ''
+    #     mkdir $out
+    #     for ext in ${toString nativeMessagingHosts}; do
+    #         ln -sLt $out $ext/lib/mozilla/native-messaging-hosts/*
+    #     done
+    #   '';
+    # };
     programs.firefox = {
       enable = true;
       # nativeMessagingHosts = [
