2dbd97366ac6346cb0e39ac78d08ff65497d2f8e
lucidph3nx <ben@tinfoilforest.nz>
2024-05-06 20:07:56 +1200

    its not crazy if it works*

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,12 +1,12 @@
 #!bash
 echo "on which disk am I setting up nixos?"
 read diskname
-device="'"/dev/${diskname}"'"
+device="/dev/${diskname}"
 
 sudo nix --experimental-features "nix-command flakes" \
     run github:nix-community/disko -- \
     --mode disko ./disko.nix \
-    --arg device $device
+    --arg device "'"\"$device\""'"
 
 sudo nixos-generate-config --no-filesystems --root /mnt
 
