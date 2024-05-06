ef403ff5614f93e630125bbe09f5d0ac175ef188
lucidph3nx <ben@tinfoilforest.nz>
2024-05-06 19:57:33 +1200

    fuck I'm dumb

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,11 +1,11 @@
 #!bash
 echo "on which disk am I setting up nixos?"
 read diskname
-let device="\'/dev/${diskname}\'"
+let device="/dev/${diskname}"
 
 sudo nix --experimental-features "nix-command flakes" \
     run github:nix-community/disko -- \
-    --mode disko ./disko.nix
+    --mode disko ./disko.nix \ 
     --arg device $device
 
 sudo nixos-generate-config --no-filesystems --root /mnt
