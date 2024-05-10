376576545fe55dd3526c5aabb47239806f9f448e
lucidph3nx <ben@tinfoilforest.nz>
2024-05-10 17:46:37 +1200

    make defaults hostname default.. shouldve done it ages ago

--- machines/default/configuration.nix
+++ machines/default/configuration.nix
@@ -89,7 +89,7 @@
     chown -R ben:users /persist/home
   '';
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "default"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Enable networking
