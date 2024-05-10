16b40f37a64cd66cf4f72b56a8f068ee093be53a
lucidph3nx <ben@tinfoilforest.nz>
2024-05-11 09:19:02 +1200

    remove old zsh autosuggestions code, no longer needed

--- modules/home-manager/cli/zsh.nix
+++ modules/home-manager/cli/zsh.nix
@@ -27,15 +27,6 @@
           src = lib.cleanSource ./files;
           file = "p10k.zsh";
         }
-        # {
-        #   name = "zsh-autosuggestions";
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "zsh-users";
-        #     repo = "zsh-autosuggestions";
-        #     rev = "v0.4.0";
-        #     sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-        #   };
-        # }
       ];
       oh-my-zsh = {
         enable = true;
