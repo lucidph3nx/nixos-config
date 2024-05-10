e1fae95d2f04352f5c694b97dbbad6a5ddb4a57b
lucidph3nx <ben@tinfoilforest.nz>
2024-05-10 17:56:05 +1200

    arm hostname

--- machines/default-arm/configuration.nix
+++ machines/default-arm/configuration.nix
@@ -87,7 +87,7 @@
     chown -R ben:users /persist/home
   '';
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "default-arm"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Enable networking
