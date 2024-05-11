fbb2354dd725ae3852ec83c7be9676e206c5875c
lucidph3nx <ben@tinfoilforest.nz>
2024-05-11 12:58:50 +1200

    starting to comment out stuff

--- modules/home-manager/firefox/userChrome.nix
+++ modules/home-manager/firefox/userChrome.nix
@@ -7,12 +7,13 @@ with config.theme;
       lib.mkEnableOption "hides the url bar in firefox by default";
   };
   config = lib.mkIf config.homeManagerModules.firefox.enable {
-    # homeManagerModules.firefox.hideUrlbar = lib.mkDefault true;
+    homeManagerModules.firefox.hideUrlbar = lib.mkDefault false;
     # heavy firefox customization
     # mostly inspired/copied from https://github.com/Dook97/firefox-qutebrowser-userchrome
     programs.firefox.profiles.main.userChrome = 
     let
-      hideUrlBar = lib.mkIf config.homeManagerModules.firefox.hideUrlbar 
+      hideUrlBar = 
+      # lib.mkIf config.homeManagerModules.firefox.hideUrlbar 
       /* css */
       ''
         #nav-bar {
