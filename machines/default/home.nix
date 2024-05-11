04ac940c2c363ffb6b3d08a94cd748401c954b7a
lucidph3nx <ben@tinfoilforest.nz>
2024-05-11 13:14:32 +1200

    syntax fix

--- machines/default/home.nix
+++ machines/default/home.nix
@@ -8,7 +8,7 @@
   # my own modules
   homeManagerModules = {
     firefox.enable = true; # testing removing and readding
-    firefox.hideUrlBar = true;
+    firefox.hideUrlbar = true;
     prospect-mail.enable = true;
     teams-for-linux.enable = true;
     # Enable home automation stuff as device should be in the home
