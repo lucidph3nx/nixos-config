7919c86e2a47aa88d3fbf729277618fe5ac696ee
lucidph3nx <ben@tinfoilforest.nz>
2024-05-11 17:56:36 +1200

    remove disk part from hw config on navi, now handled by disko

--- machines/navi/hardware-configuration.nix
+++ machines/navi/hardware-configuration.nix
@@ -13,20 +13,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/e2849eb8-a088-40ea-86d1-559b18f1ca11";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/DFE8-B1C1";
-      fsType = "vfat";
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/57bdf9ec-73cb-4e67-a78d-ce06cacee412"; }
-    ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
