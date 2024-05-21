226f43fcf665eb9d98fbb5c58cd9f2bb6c7ec5fb
lucidph3nx <ben@tinfoilforest.nz>
2024-05-21 17:57:46 +1200

    auto mount nfs, not necessarily while booting

--- modules/nix/nfs-mounts.nix
+++ modules/nix/nfs-mounts.nix
@@ -11,42 +11,52 @@ in
     fileSystems."/nfs/3d" = {
       device = "${nasServerIP}:/volume1/3d";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     fileSystems."/nfs/ben-backup" = {
       device = "${nasServerIP}:/volume1/ben-backup";
-        fsType = "nfs";
+      fsType = "nfs";
+      options = [ "x-systemd.automount" ];
       };
     fileSystems."/nfs/books" = {
       device = "${nasServerIP}:/volume1/Books";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     fileSystems."/nfs/downloads" = {
       device = "${nasServerIP}:/volume1/downloads";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     fileSystems."/nfs/learn" = {
       device = "${nasServerIP}:/volume1/learn";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     fileSystems."/nfs/longhorn-backups" = {
       device = "${nasServerIP}:/volume1/longhorn-backups";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     fileSystems."/nfs/movies" = {
       device = "${nasServerIP}:/volume1/Films";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     fileSystems."/nfs/music" = {
       device = "${nasServerIP}:/volume1/Music";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     fileSystems."/nfs/tv" = {
       device = "${nasServerIP}:/volume1/TV-Series";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
     fileSystems."/nfs/software" = {
       device = "${nasServerIP}:/volume1/Software";
       fsType = "nfs";
+      options = [ "x-systemd.automount" ];
     };
   };
 }
