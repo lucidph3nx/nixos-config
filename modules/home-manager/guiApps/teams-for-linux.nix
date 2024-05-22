0f935cf6607dcd623a960794a7667be567cc5502
lucidph3nx <ben@tinfoilforest.nz>
2024-05-22 17:56:02 +1200

    abandon configuring teams-for-linux via home manager

--- modules/home-manager/guiApps/teams-for-linux.nix
+++ modules/home-manager/guiApps/teams-for-linux.nix
@@ -7,11 +7,34 @@
   };
   config = lib.mkIf config.homeManagerModules.teams-for-linux.enable {
     home.packages = with pkgs; [ teams-for-linux ];
+
+    # teams-for-linux does not seem to read config files that are symlinks
+    # home.file = {
+    #   ".config/teams-for-linux/config.json" = {
+    #     text = /* json */''
+    #       {
+    #         "spellCheckerLanguages":["en-NZ"],
+    #         "optInTeamsV2":"true",
+    #         "customCSSLocation":"/home/ben/.config/teams-for-linux/custom.css"
+    #       }
+    #     '';
+    #   };
+    # };
+    # home.file = {
+    #   ".config/teams-for-linux/custom.css" = {
+    #     text = /* css */''
+    #       html, body {
+    #         font-family: JetBrains Mono, monospace !important;
+    #       }
+    #     '';
+    #   };
+    # };
+
     xdg.desktopEntries = lib.mkIf pkgs.stdenv.isLinux {
         teams-for-linux = {
           name = "Teams";
           genericName = "teams";
-          exec = ''teams-for-linux --spellCheckerLanguages="en-NZ" %U'';
+          exec = ''teams-for-linux %U'';
           icon = "teams";
           categories = [ "Network" "Chat" "InstantMessaging"];
           mimeType = ["x-scheme-handler/msteams"];
