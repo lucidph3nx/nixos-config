24494f3e54dd9c232f3ac501e012bfe86df58d60
lucidph3nx <ben@tinfoilforest.nz>
2024-05-22 20:21:02 +1200

    formatting

--- modules/home-manager/cli/asdf.nix
+++ modules/home-manager/cli/asdf.nix
@@ -10,8 +10,8 @@
       pkgs.asdf-vm
     ];
     home.file.".tool-versions".text = ''
-    nodejs 20.8.1
-    terraform 1.3.6
+      nodejs 20.8.1
+      terraform 1.3.6
     '';
     programs.zsh.initExtra = ''
       . ${pkgs.asdf-vm}/share/asdf-vm/asdf.sh
