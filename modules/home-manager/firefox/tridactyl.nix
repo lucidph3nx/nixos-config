c8a43a886849693990e2b0800443a5403dfd8d98
lucidph3nx <ben@tinfoilforest.nz>
2024-05-23 09:58:52 +1200

    set up searchurl for youtube in tridactyl

--- modules/home-manager/firefox/tridactyl.nix
+++ modules/home-manager/firefox/tridactyl.nix
@@ -42,6 +42,7 @@
         " Search Urls
         setnull searchurls.github
         set searchurls.gh https://github.com/search?q=
+        set searchurls.yt https://www.youtube.com/results?search_query=
         set searchurls.hm https://home-manager-options.extranix.com/?query=
         set searchurls.nixpkgs https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=
         set searchurls.nixopt https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=
