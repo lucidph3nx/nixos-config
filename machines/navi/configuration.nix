28024e974f32ea2ea732022b05235a9c010325ca
lucidph3nx <ben@tinfoilforest.nz>
2024-07-24 14:21:25 +1200

    remove unneeded gaming things

--- machines/navi/configuration.nix
+++ machines/navi/configuration.nix
@@ -280,8 +280,6 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = [ pkgs.amdvlk ];
-    extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
   };
   services.xserver.videoDrivers = ["amdgpu"];
   programs.steam = {
