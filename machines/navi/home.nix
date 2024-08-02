ddf52592eaa45468bbd5c8e4831aff4b4f0092cc
lucidph3nx <ben@tinfoilforest.nz>
2024-08-03 06:54:02 +1200

    cleanup comment

--- machines/navi/home.nix
+++ machines/navi/home.nix
@@ -39,7 +39,6 @@
         directory = ".local/share/Steam";
         method = "symlink";
       }
-      # ".local/share/Steam"
       ".local/share/mpd"
       ".local/share/nvim"
       ".local/share/vulkan"
