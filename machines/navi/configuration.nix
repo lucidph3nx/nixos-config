5761c6481e2abe5607af27ac93b9a959a83f8679
lucidph3nx <ben@tinfoilforest.nz>
2024-08-19 08:14:18 +1200

    add anki

--- machines/navi/configuration.nix
+++ machines/navi/configuration.nix
@@ -176,6 +176,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    anki
     exfat
     fastfetch
     ffmpeg
