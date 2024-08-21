7cb9a8fad8ab44237ca384b2ff7cab65a2b925ba
lucidph3nx <ben@tinfoilforest.nz>
2024-08-21 20:02:07 +1200

    cleanup comments

--- machines/navi/configuration.nix
+++ machines/navi/configuration.nix
@@ -316,12 +316,6 @@
   services.syncthing.cert = config.sops.secrets.navi-cert-pem.path;
   services.syncthing.key = config.sops.secrets.navi-key-pem.path;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
