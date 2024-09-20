a3792122cd6d07e913d244da3af6276e2149fc34
lucidph3nx <ben@tinfoilforest.nz>
2024-09-21 09:19:46 +1200

    fix pipewire audio output switcher scripts

--- modules/home-manager/desktopEnvironment/pipewire-utils.nix
+++ modules/home-manager/desktopEnvironment/pipewire-utils.nix
@@ -23,8 +23,8 @@
           # Find the ID of the current default sink
           current_sink_id=$(wpctl inspect @DEFAULT_SINK@ | awk 'NR==1 {gsub(/,/, "", $2); print $2}')
 
-          # Get the IDs of all sinks with priority.session == 1009
-          all_sink_ids=$(pw-dump | jq -r '.[] | select(.type == "PipeWire:Interface:Node" and .info.props."media.class" == "Audio/Sink" and .info.props."priority.session" == 1009) | .id')
+          # Get the IDs of all sinks with priority.session >= 1000
+          all_sink_ids=$(pw-dump | jq -r '.[] | select(.type == "PipeWire:Interface:Node" and .info.props."media.class" == "Audio/Sink" and .info.props."priority.session" >= 1000) | .id')
 
           # Filter out the current sink ID
           next_sink_id=$(echo "$all_sink_ids" | grep -v "$current_sink_id" | head -n 1)
@@ -47,6 +47,7 @@
               declare -A alt_lookup
               alt_lookup=(
                   ["Razer BlackShark V2 Pro"]="󰋋"
+                  ["Razer Nari"]="󰋋"
                   ["ALC1220 Analog"]="󰓃"
                   # Add more entries here as needed
               )
