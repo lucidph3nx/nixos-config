6c7802acc0e09594c1fffb1d3beb2a0a696f2745
lucidph3nx <ben@tinfoilforest.nz>
2024-09-22 12:45:02 +1200

    key remap for surface

--- machines/surface/configuration.nix
+++ machines/surface/configuration.nix
@@ -161,6 +161,24 @@
     };
   };
 
+  # remap caps to esc and control
+  services.interception-tools =
+  let
+    itools = pkgs.interception-tools;
+    itools-caps = pkgs.interception-tools-plugins.caps2esc;
+  in
+  {
+    enable = true;
+    plugins = [ itools-caps ];
+    # requires explicit paths: https://github.com/NixOS/nixpkgs/issues/126681
+    udevmonConfig = pkgs.lib.mkDefault ''
+      - JOB: "${itools}/bin/intercept -g $DEVNODE | ${itools-caps}/bin/caps2esc -m 1 | ${itools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+  };
+
   # List services that you want to enable:
   services.dbus.implementation = "broker";
   # Enable the OpenSSH daemon.
