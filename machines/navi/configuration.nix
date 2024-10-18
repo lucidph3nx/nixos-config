36c8aff6efd1c927476b3024956d878bc2065287
lucidph3nx <ben@tinfoilforest.nz>
2024-10-19 09:15:42 +1300

    add cheese, more convienient for taking photos from usb microscope

--- machines/navi/configuration.nix
+++ machines/navi/configuration.nix
@@ -180,6 +180,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    cheese
     exfat
     fastfetch
     ffmpeg
