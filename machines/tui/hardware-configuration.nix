fb760eac25c39abd3431ccebe939ece7b34c8750
lucidph3nx <ben@tinfoilforest.nz>
2024-11-14 22:04:42 +1300

    up to date kernel very important for tui hardware

--- machines/tui/hardware-configuration.nix
+++ machines/tui/hardware-configuration.nix
@@ -16,6 +16,7 @@
   boot.initrd.kernelModules = ["dm-snapshot"];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
