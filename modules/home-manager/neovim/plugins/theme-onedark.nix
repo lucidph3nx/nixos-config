8d58c2bf4f0c869125741ca3b38f1aefa0c94854
lucidph3nx <ben@tinfoilforest.nz>
2024-11-18 18:33:34 +1300

    add some setup for onedark theme in nvim

--- modules/home-manager/neovim/plugins/theme-onedark.nix
+++ modules/home-manager/neovim/plugins/theme-onedark.nix
@@ -13,6 +13,7 @@
       hash = "sha256-NLHq9SUUo81m50NPQe8852uZbo4Mo4No10N3ptX43t0=";
     };
   };
+  theme = config.theme;
 in {
   programs.neovim.plugins = lib.mkIf (config.theme.name == "onedark") [
     {
@@ -23,7 +24,45 @@ in {
         lua
         */
         ''
-          require('onedark').load()
+          require('onedark').setup {
+            highlights = {
+              ObsidianTodo = {
+                fg = '${theme.purple}',
+                bg = 'none',
+                bold = true
+              },
+              ObsidianDone = {
+                fg = '${theme.green}',
+                bg = 'none',
+                bold = true
+              },
+              ObsidianRightArrow = {
+                fg = '${theme.purple}',
+                bg = 'none',
+                bold = true
+              },
+              ObsidianTilde = {
+                fg = '${theme.orange}',
+                bg = 'none',
+                bold = true
+              },
+              ObsidianRefText = {
+                fg = '${theme.blue}',
+                bg = 'none',
+                bold = true
+              },
+              ObsidianExtLinkIcon = {
+                fg = '${theme.blue}',
+                bg = 'none',
+              },
+              ObsidianTag = {
+                fg = '${theme.blue}',
+                bg = 'none',
+                italic = true
+              },
+
+            }
+          }
           vim.cmd('colorscheme onedark')
         '';
     }
