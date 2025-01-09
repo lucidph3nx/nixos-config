3a9b0418a115a096af788a0a0758782bec6ac02f
lucidph3nx <ben@tinfoilforest.nz>
2025-01-10 08:40:22 +1300

    more reddit element blocking

--- modules/home-manager/qutebrowser/greasemonkey/reddit_adblock.js
+++ modules/home-manager/qutebrowser/greasemonkey/reddit_adblock.js
@@ -4,13 +4,17 @@
 // ==/UserScript==
 
 const removeElements = () => {
-    // promoted posts has this class
+    // promoted posts
     document.querySelectorAll('.promotedlink').forEach((e) => e.remove());
+    // redesign
+    document.querySelectorAll('.redesign-beta-optin').forEach((e) => e.remove());
+    // premium ads
+    document.querySelectorAll('.premium-banner-outer').forEach((e) => e.remove());
     // nagging about confirming email has this class
     document
         .querySelectorAll('.kEQVd8aneM1tVkcIKUyDT')
         .forEach((e) => e.remove());
 };
 (trySetInterval = () => {
-    window.setInterval(removeElements, 1000);
+    window.setInterval(removeElements, 100);
 })();
