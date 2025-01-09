d3d66ce555cef4026172b9307e8ef1442cdb70c7
lucidph3nx <ben@tinfoilforest.nz>
2025-01-10 11:26:40 +1300

    quicker reddit adblocker (no interval)

--- modules/home-manager/qutebrowser/greasemonkey/reddit_adblock.js
+++ modules/home-manager/qutebrowser/greasemonkey/reddit_adblock.js
@@ -3,18 +3,9 @@
 // @match        *://*.reddit.com/*
 // ==/UserScript==
 
-const removeElements = () => {
-    // promoted posts
-    document.querySelectorAll('.promotedlink').forEach((e) => e.remove());
-    // redesign
-    document.querySelectorAll('.redesign-beta-optin').forEach((e) => e.remove());
-    // premium ads
-    document.querySelectorAll('.premium-banner-outer').forEach((e) => e.remove());
-    // nagging about confirming email has this class
-    document
-        .querySelectorAll('.kEQVd8aneM1tVkcIKUyDT')
-        .forEach((e) => e.remove());
-};
-(trySetInterval = () => {
-    window.setInterval(removeElements, 100);
-})();
+// promoted posts
+document.querySelectorAll('.promotedlink').forEach((e) => e.remove());
+// redesign
+document.querySelectorAll('.redesign-beta-optin').forEach((e) => e.remove());
+// premium ads
+document.querySelectorAll('.premium-banner-outer').forEach((e) => e.remove());
