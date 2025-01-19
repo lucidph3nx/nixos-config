f8640f3170c4364e0adef5b5eb85fbed8676e622
lucidph3nx <ben@tinfoilforest.nz>
2025-01-20 10:19:37 +1300

    switch to 1GB boot volume for tui machine

--- machines/tui/disko.nix
+++ machines/tui/disko.nix
@@ -14,7 +14,7 @@
           };
           esp = {
             name = "ESP";
-            size = "500M";
+            size = "1G";
             type = "EF00";
             content = {
               type = "filesystem";
