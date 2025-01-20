967cd98854103c91654dc2a9b97c2c5b453a43d3
lucidph3nx <ben@tinfoilforest.nz>
2025-01-21 12:19:33 +1300

    some more styling for gcal

--- modules/home-manager/qutebrowser/greasemonkey/googlecalendar.css.js
+++ modules/home-manager/qutebrowser/greasemonkey/googlecalendar.css.js
@@ -20,5 +20,9 @@ GM_addStyle(`
 
   body {
     font-family: JetBrains Mono, monospace !important;
+    --gm3-sys-color-surface-container-low: var(--system-theme-bg_dim) !important;
+    --gm3-sys-color-surface: var(--system-theme-bg0) !important;
+    --gm3-sys-color-surface-container-highest: var(--system-theme-bg3) !important;
+    --gm3-sys-color-on-surface: var(--system-theme-fg) !important;
   }
 `)
