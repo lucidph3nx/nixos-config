314ccb26ca1cd20d67a26d16415176b69270f6d3
lucidph3nx <ben@tinfoilforest.nz>
2025-01-22 14:18:31 +1300

    allow conform to format languages injected inside nix

--- modules/home-manager/neovim/plugins/conform.nix
+++ modules/home-manager/neovim/plugins/conform.nix
@@ -15,22 +15,19 @@
         lua
         */
         ''
-          require('conform').setup {
-            formatters_by_ft = {
-              lua = { "stylua" },
-              python = { "black" },
-              nix = { "alejandra" },
-              json = { "jq" },
-              terraform = { "terraform_fmt" },
-            }
-          }
+          require("conform").setup({
+          	formatters_by_ft = {
+          		lua = { "stylua" },
+          		python = { "black" },
+          		nix = { "alejandra", "injected" },
+          		json = { "jq" },
+          		terraform = { "terraform_fmt" },
+          	},
+          })
           -- Keybindings
-          vim.keymap.set(
-            'n', '<leader>f',
-            function()
-              require("conform").format({ async = true, lsp_fallback = true })
-            end,
-            { desc = "Format the current buffer" })
+          vim.keymap.set("n", "<leader>f", function()
+          	require("conform").format({ async = true, lsp_fallback = true })
+          end, { desc = "Format the current buffer" })
         '';
     }
   ];
