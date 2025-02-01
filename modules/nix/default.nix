3c26698facbfe74bcf7eb54c7e38ff95a62c01e1
lucidph3nx <ben@tinfoilforest.nz>
2025-02-02 11:27:26 +1300

    add in automatic garbage collection and nix store optimisation

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -31,16 +31,26 @@
   };
   config = {
     # nix options for all systems
-    nix.settings = {
-      experimental-features = ["nix-command" "flakes"];
-      trusted-substituters = [
-        "https://nix-community.cachix.org"
-        "https://lucidph3nx-nixos-config.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "lucidph3nx-nixos-config.cachix.org-1:gXiGMMDnozkXCjvOs9fOwKPZNIqf94ZA/YksjrKekHE="
-      ];
+    nix = {
+      settings = {
+        experimental-features = ["nix-command" "flakes"];
+        trusted-substituters = [
+          "https://nix-community.cachix.org"
+          "https://lucidph3nx-nixos-config.cachix.org"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "lucidph3nx-nixos-config.cachix.org-1:gXiGMMDnozkXCjvOs9fOwKPZNIqf94ZA/YksjrKekHE="
+        ];
+        # remove duplicate store paths
+        auto-optimise-store = true;
+      };
+      # automatic garbage collection
+      gc = {
+        automatic = true;
+        dates = "daily";
+        options = "--delete-older-than 15d";
+      };
     };
     # increase network buffer size
     boot.kernel.sysctl."net.core.rmem_max" = 2500000;
