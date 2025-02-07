92166104ad98694fd6940cf285ed030d3966e775
lucidph3nx <ben@tinfoilforest.nz>
2025-02-07 16:42:52 +1300

    move scripts claiming to be for pipewire that are actually for waybar

--- modules/home-manager/desktopEnvironment/pipewire-utils.nix
+++ modules/home-manager/desktopEnvironment/pipewire-utils.nix
@@ -33,57 +33,5 @@
     #       wpctl set-default $next_sink_id
     #     '';
     # };
-    home.file.".local/scripts/cli.audio.outputEnabled" = {
-      executable = true;
-      text =
-        /*
-        bash
-        */
-        ''
-          #!/bin/sh
-          # Find the nick of the current default sink
-          current_sink_=$(wpctl inspect @DEFAULT_SINK@ | grep -m 1 'node.nick' | awk -F ' = ' '{gsub(/"/, "", $2); print $2}')
-
-          if [ "$current_sink_" = "FiiO K7" ]; then
-              echo "󰕾"
-          else
-              echo "󰖁"
-          fi
-        '';
-    };
-    home.file.".local/scripts/cli.audio.getOutput" = {
-      executable = true;
-      text =
-        /*
-        bash
-        */
-        ''
-          #!/bin/sh
-          # a handy little lookup table to provide icons
-          get_alt() {
-              local text="$1"
-              declare -A alt_lookup
-              alt_lookup=(
-                  ["Razer BlackShark V2 Pro"]="󰋋"
-                  ["Razer Nari"]="󰋋"
-                  ["ALC1220 Analog"]="󰓃"
-                  ["FiiO K7"]="󰤽"
-                  # Add more entries here as needed
-              )
-              echo ''${alt_lookup[$text]}
-          }
-
-          # Find the ID of the current default sink
-          current_sink_id=$(wpctl inspect @DEFAULT_SINK@ | awk 'NR==1 {gsub(/,/, "", $2); print $2}')
-
-          # Get the node.nick of the current default sink
-          current_sink_nick=$(pw-dump | jq -r '.[] | select(.id == '$current_sink_id') | .info.props."node.nick"')
-          alt=$(get_alt "$current_sink_nick")
-
-          # Format the output as JSON
-          output=$(printf '{"text": "%s", "alt": "%s", "tooltip": "%s"}' "$current_sink_nick" "$alt" "$current_sink_nick")
-          echo "$output"
-        '';
-    };
   };
 }
