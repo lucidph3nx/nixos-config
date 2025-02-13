96839d101758f49147c91534aa15ae2def673d75
lucidph3nx <ben@tinfoilforest.nz>
2025-02-13 20:02:15 +1300

    persist photos

--- modules/services/syncthing/default.nix
+++ modules/services/syncthing/default.nix
@@ -119,6 +119,7 @@
       allowOther = true;
       directories = [
         ".config/syncthing"
+        (lib.mkIf config.nx.services.syncthing.photos.enable "pictures/photos")
       ];
     };
   };
