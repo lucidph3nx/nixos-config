775bcbc9a799537bcb1e69f87376a991aac4d502
lucidph3nx <ben@tinfoilforest.nz>
2025-02-25 14:57:19 +1300

    wayland obsidian app

--- modules/programs/obsidian.nix
+++ modules/programs/obsidian.nix
@@ -12,14 +12,24 @@
       };
   };
   config = lib.mkIf config.nx.programs.obsidian.enable {
-    home-manager.users.ben.home = {
-      packages = with pkgs; [
-        obsidian
-      ];
-      persistence."/persist/home/ben" = {
-        directories = [
-          ".config/obsidian"
+    home-manager.users.ben = {
+      home = {
+        packages = with pkgs; [
+          obsidian
         ];
+        persistence."/persist/home/ben" = {
+          directories = [
+            ".config/obsidian"
+          ];
+        };
+      };
+      # force wayland
+      xdg.desktopEntries.obsidian = {
+        name = "Obsidian";
+        comment = "Knowledge base";
+        icon = "obsidian";
+        exec = "obsidian --enable-features=UseOzonePlatform --ozone-platform=wayland";
+        mimeType = ["x-scheme-handler/obsidian"];
       };
     };
   };
