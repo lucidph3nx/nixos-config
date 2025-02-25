422ef30c29e05101fe47b4962a92b3997e651c7b
lucidph3nx <ben@tinfoilforest.nz>
2025-02-25 13:10:22 +1300

    add separate module for powerprofilesdaemon and persist the directory

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -6,15 +6,13 @@
     ./mpd.nix
     ./pipewire.nix
     ./polkit.nix
+    ./powerProfilesDaemon.nix
     ./printer.nix
     ./syncthing
     ./udiskie.nix
   ];
   config = {
     # default service configuration, things that don't need their own module
-
-    # power management
-    services.power-profiles-daemon.enable = true;
 
     # Enable the OpenSSH daemon.
     services.openssh.enable = true;
---
+++ modules/services/powerProfilesDaemon.nix
@@ -0,0 +1,26 @@
+
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  options = {
+    nx.services.powerProfilesDaemon.enable =
+      lib.mkEnableOption "Add power-profiles-daemon setup"
+      // {
+        default = true;
+      };
+  };
+  config = lib.mkIf config.nx.services.powerProfilesDaemon.enable {
+    environment.persistence."/persist/system" = {
+      hideMounts = true;
+      directories = [
+        "/var/lib/power-profiles-daemon" # remember power profile (set using powerprofilesctl set <profile>)
+      ];
+    };
+    services.power-profiles-daemon.enable = true;
+  };
+}
+
+
