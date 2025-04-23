f17b71a46591b4b5a3899643e56faf098201f53f
lucidph3nx <ben@tinfoilforest.nz>
2025-04-23 20:26:15 +1200

    fix git blame hiding in zen-mode.nvim

--- modules/programs/neovim/plugins/zenmode.nix
+++ modules/programs/neovim/plugins/zenmode.nix
@@ -17,12 +17,27 @@
               },
             },
           	plugins = {
+              gitsigns = { enabled = false },
           		tmux = { enabled = true },
           		kitty = {
           			enabled = true,
           			font = "+2",
           		},
           	},
+            on_open = function(win)
+              local gs = require("gitsigns")
+              local config = require("gitsigns.config").config
+              config.current_line_blame = false
+              config.current_line_blame_opts.virt_text = false
+              gs.refresh()
+            end,
+            on_close = function(win)
+              local gs = require("gitsigns")
+              local config = require("gitsigns.config").config
+              config.current_line_blame = true
+              config.current_line_blame_opts.virt_text = true
+              gs.refresh()
+            end,
           })
           vim.keymap.set("n", "<leader>z", vim.cmd.ZenMode, { desc = "[Z]enMode" })
         '';
