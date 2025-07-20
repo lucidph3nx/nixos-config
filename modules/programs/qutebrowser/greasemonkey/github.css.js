3c10661d19c1c1fdae320de2c74de457636471dc
lucidph3nx <ben@tinfoilforest.nz>
2025-07-21 09:53:20 +1200

    github css fix primary button hover colour

--- modules/programs/qutebrowser/greasemonkey/github.css.js
+++ modules/programs/qutebrowser/greasemonkey/github.css.js
@@ -43,6 +43,7 @@ GM_addStyle(`
     --fgColor-onEmphasis: var(--system-theme-bg0) !important;
     --button-primary-fgColor-resting: var(--system-theme-bg0) !important;
     --button-primary-iconColor-rest: var(--system-theme-bg0) !important;
+    --button-primary-bgColor-hover: color-mix(in srgb, var(--system-theme-green) 75%, var(--system-theme-bg0) 25%) !important;
 
     --bgColor-default: var(--system-theme-bg0) !important;
     --bgColor-inset: var(--system-theme-bg_dim) !important;
