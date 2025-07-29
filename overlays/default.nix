c20006a2bcaee4b624d6c2cddb6ac52173d5bb50
lucidph3nx <ben@tinfoilforest.nz>
2025-07-29 20:23:18 +1200

    clarify reason for darktable overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,7 +10,9 @@ in rec {
   modifications = final: prev: {
     # without this, darktable fails to build
     # fix is merged, waiting for https://nixpk.gs/pr-tracker.html?pr=424658
-    # new issues, ?? leaving this here for now
+    # new issues https://github.com/NixOS/nixpkgs/issues/429268
+    # it seems like libsoup is used by osm-gps-map which is used by darktable
+    # and we are all mad at libsoup 2, because its unmaintained and riddled with security issues
     darktable = inputs.nixpkgs-darktablejuly25.legacyPackages.${final.system}.darktable;
 
     # waiting for https://nixpk.gs/pr-tracker.html?pr=429146
