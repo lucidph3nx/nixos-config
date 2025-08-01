77f7756e42a0748042e406ef45b17609602e9bc4
lucidph3nx <ben@tinfoilforest.nz>
2025-08-02 11:34:02 +1200

    remove no longer needed overlay for ncmpcpp

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,8 +14,5 @@ in rec {
     # it seems like libsoup is used by osm-gps-map which is used by darktable
     # and we are all mad at libsoup 2, because its unmaintained and riddled with security issues
     darktable = inputs.nixpkgs-darktablejuly25.legacyPackages.${final.system}.darktable;
-
-    # waiting for https://nixpk.gs/pr-tracker.html?pr=429146
-    ncmpcpp = inputs.nixpkgs-master.legacyPackages.${final.system}.ncmpcpp;
   };
 }
