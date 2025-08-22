d4540d5e58a417048c4d03c95e9f964b3991ed9f
lucidph3nx <ben@tinfoilforest.nz>
2025-08-22 20:10:09 +1200

    reduce kitty padding

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -23,7 +23,8 @@ with config.theme; {
           symbol_map U+1f636,U+200D,U+1F32B,U+FE0F Noto Color Emoji
           prefer_color_emoji yes
           hide_window_decorations titlebar-only
-          window_margin_width 15
+          window_margin_width 10
+          window_padding_width 0
           confirm_os_window_close 0
           background_opacity 0.8
           enable_audio_bell no
