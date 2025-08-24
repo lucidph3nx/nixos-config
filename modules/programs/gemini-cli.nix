1bd2a66f7838d8d808cb3dad6079d8bc900735b4
lucidph3nx <ben@tinfoilforest.nz>
2025-08-24 20:36:05 +1200

    configuration for gemini-cli

--- modules/programs/gemini-cli.nix
+++ modules/programs/gemini-cli.nix
@@ -21,6 +21,44 @@
           ".gemini"
         ];
       };
+      home.file.".gemini/settings.json" = {
+        text = builtins.toJSON ({
+          # configuration options can be found here:
+          # https://github.com/google-gemini/gemini-cli/blob/main/docs/cli/configuration.md
+          selectedAuthType = "oauth-personal";
+          contextFileName = "AGENTS.md";
+          preferredEditor = "neovim";
+          vimMode = true;
+          theme = "custom";
+          customThemes = with config.theme; {
+            custom = {
+              name = "custom";
+              type = "custom";
+              Background = "${bg0}";
+              Foreground = "${foreground}";
+              LightBlue = "${primary}";
+              AccentBlue = "${blue}";
+              AccentPurple = "${purple}";
+              AccentCyan = "${aqua}";
+              AccentGreen = "${green}";
+              AccentYellow = "${yellow}";
+              AccentRed = "${red}";
+              Comment = "${grey1}";
+              Gray = "${grey1}";
+              DiffAdded = "${green}";
+              DiffRemoved = "${red}";
+              DiffModified = "${blue}";
+              GradientColors = [
+                "${red}"
+                "${yellow}"
+                "${green}"
+                "${blue}"
+                "${purple}"
+              ];
+            };
+          };
+        });
+      };
     };
   };
 }
