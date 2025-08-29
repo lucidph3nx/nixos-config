4bc5ca4ab96870ad02995993bddc175eb2a8cec7
lucidph3nx <ben@tinfoilforest.nz>
2025-08-29 17:44:05 +1200

    refilne opencode config

--- modules/programs/opencode.nix
+++ modules/programs/opencode.nix
@@ -12,20 +12,13 @@
   };
   config = lib.mkIf config.nx.programs.opencode.enable {
     home-manager.users.ben = {
-      home.packages = with pkgs; [
-        opencode
-      ];
-      home.persistence."/persist/home/ben" = {
-        directories = [
-          ".config/opencode"
-        ];
-      };
-      home.file.".config/opencode/opencode.jsonc" = {
-        text = builtins.toJSON ({
+      programs.opencode = {
+        enable = true;
+        settings = {
           theme = "everforest";
           permission = {
             edit = "allow";
-            "webfetch" = "allow";
+            webfetch = "allow";
             bash = {
               "git push" = "ask";
               "git commit" = "ask";
@@ -34,7 +27,12 @@
               "*" = "ask";
             };
           };
-        });
+        };
+      };
+      home.persistence."/persist/home/ben" = {
+        directories = [
+          ".config/opencode"
+        ];
       };
     };
   };
