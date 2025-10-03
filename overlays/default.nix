c2694a3043b530e3a4782ed1d10253c662295bf0
lucidph3nx <ben@tinfoilforest.nz>
2025-10-03 14:39:10 +1300

    use master for fluxcd cli for now

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,5 +14,8 @@ rec {
     # use master for gemini-cli - things move quickly
     gemini-cli = inputs.nixpkgs-master.legacyPackages.${final.system}.gemini-cli;
     opencode = inputs.nixpkgs-master.legacyPackages.${final.system}.opencode;
+
+    # user master for fluxcd, waiting for https://nixpk.gs/pr-tracker.html?pr=447904
+    fluxcd = inputs.nixpkgs-master.legacyPackages.${final.system}.fluxcd;
   };
 }
