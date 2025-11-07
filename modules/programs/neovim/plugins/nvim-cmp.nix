cbefa91cbcef56cda0fa9e13e749e938b979ab74
lucidph3nx <ben@tinfoilforest.nz>
2025-11-07 15:29:07 +1300

    remove copilot from nvim-cmp

--- modules/programs/neovim/plugins/nvim-cmp.nix
+++ modules/programs/neovim/plugins/nvim-cmp.nix
@@ -56,7 +56,6 @@
           		end, { "i", "s" }),
           	}),
           	sources = {
-          		{ name = "copilot", group_index = 2 },
           		{ name = "nvim_lsp", group_index = 2 },
           		{ name = "luasnip", group_index = 2 },
           	},
