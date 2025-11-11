732b54227e49061624bed5373da3217541983383
lucidph3nx <ben@tinfoilforest.nz>
2025-11-11 18:05:03 +1300

    temp switch to discord to get around electron36 deprecation

--- modules/programs/webcord.nix
+++ modules/programs/webcord.nix
@@ -13,11 +13,15 @@
   config = lib.mkIf config.nx.programs.webcord.enable {
     home-manager.users.ben = {
       home.packages = with pkgs; [
-        webcord
+        # webcord
+        # on 2025-11-11 webcord stopped building due to using an old electron version
+        # so I've temporarily switched to discord
+        discord
       ];
       home.persistence."/persist/home/ben" = {
         directories = [
           ".config/WebCord"
+          ".config/discord"
         ];
       };
       wayland.windowManager.hyprland.settings =
