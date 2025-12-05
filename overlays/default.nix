dd46f38485cf9be7f3725ba5fc469df24b83f48d
lucidph3nx <ben@tinfoilforest.nz>
2025-12-06 09:34:07 +1300

    remove lualine overlay, now fixed

--- overlays/default.nix
+++ overlays/default.nix
@@ -46,23 +46,6 @@ rec {
           hash = "sha256-hG1Dji1Xrl7yLLB5nN7twCuldIZt6GYWwJJaYCcGTqc=";
         };
       };
-      # broken on 2025-11-28, supposedly fixed in https://github.com/NixOS/nixpkgs/pull/464616
-      # however, that should be merged and in unstable but its still an issue
-      lualine-nvim = prev.neovimUtils.buildNeovimPlugin {
-        luaAttr = prev.luaPackages.lualine-nvim.overrideAttrs {
-          knownRockspec =
-            (prev.fetchurl {
-              url = "mirror://luarocks/lualine.nvim-scm-1.rockspec";
-              sha256 = "01cqa4nvpq0z4230szwbcwqb0kd8cz2dycrd764r0z5c6vivgfzs";
-            }).outPath;
-          src = prev.fetchFromGitHub {
-            owner = "nvim-lualine";
-            repo = "lualine.nvim";
-            rev = "47f91c416daef12db467145e16bed5bbfe00add8";
-            hash = "sha256-OpLZH+sL5cj2rcP5/T+jDOnuxd1QWLHCt2RzloffZOA=";
-          };
-        };
-      };
     };
   };
 }
