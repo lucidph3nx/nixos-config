68a263c9d0aa5e83f5625ac0b34dfad8196d52fd
lucidph3nx <ben@tinfoilforest.nz>
2025-12-06 09:02:20 +1300

    overlay to pin kernel while waiting for a pr to make it into unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -17,6 +17,10 @@ rec {
     # use older nixpkgs for calibre to avoid hipblaslt memory issues during build
     calibre = inputs.nixpkgs-stable.legacyPackages.${final.system}.calibre;
 
+    # Temporarily pin linuxPackages_latest to 6.17 due to v4l2loopback incompatibility with 6.18+
+    # waiting for https://nixpk.gs/pr-tracker.html?pr=467572
+    linuxPackages_latest = prev.linuxPackages_6_17;
+
     vimPlugins = prev.vimPlugins // {
       # There is a fix for this issue, but its aparently not in unstable yet
       # https://github.com/obsidian-nvim/obsidian.nvim/issues/387
