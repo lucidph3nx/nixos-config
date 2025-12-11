7f272915fd6fab7a28ec47b767052f449bd3d75f
lucidph3nx <ben@tinfoilforest.nz>
2025-12-12 08:49:30 +1300

    pin firefox to stable due to failing build

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,6 +11,9 @@ let
 in
 rec {
   modifications = final: prev: {
+    # use stable for firefox, unstable is currently failing to build
+    firefox = inputs.nixpkgs-stable.legacyPackages.${final.stdenv.hostPlatform.system}.firefox;
+
     # use master for opencode, need the bleeding edge
     opencode = inputs.nixpkgs-master.legacyPackages.${final.stdenv.hostPlatform.system}.opencode;
 
