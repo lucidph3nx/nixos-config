f1fa92fa8ce82854483a95e047eab597f77888f4
lucidph3nx <ben@tinfoilforest.nz>
2025-12-19 18:59:28 +1300

    remove unused import

--- modules/programs/neovim/plugins/theme-catpuccin.nix
+++ modules/programs/neovim/plugins/theme-catpuccin.nix
@@ -4,23 +4,22 @@
   config,
   ...
 }:
-let
-  theme = config.theme;
-in
 {
-  home-manager.users.ben.programs.neovim.plugins = lib.mkIf (config.theme.name == "catppuccin-latte") [
-    {
-      plugin = pkgs.vimPlugins.catppuccin-nvim;
-      type = "lua";
-      config =
-        # lua
-        ''
-          require("catppuccin").setup({
-            flavour = "latte",
-            transparent_background = true,
-          })
-          vim.cmd("colorscheme catppuccin")
-        '';
-    }
-  ];
+  home-manager.users.ben.programs.neovim.plugins =
+    lib.mkIf (config.theme.name == "catppuccin-latte")
+      [
+        {
+          plugin = pkgs.vimPlugins.catppuccin-nvim;
+          type = "lua";
+          config =
+            # lua
+            ''
+              require("catppuccin").setup({
+              	flavour = "latte",
+              	transparent_background = true,
+              })
+              vim.cmd("colorscheme catppuccin")
+            '';
+        }
+      ];
 }
