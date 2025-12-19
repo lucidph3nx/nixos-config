965a4f6f2af85d6762339a3dfff7ed41492db2b0
lucidph3nx <ben@tinfoilforest.nz>
2025-12-19 18:12:50 +1300

    remove version pinning for obsidian-nvim

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,30 +16,5 @@ rec {
 
     # use master for opencode, need the bleeding edge
     opencode = inputs.nixpkgs-master.legacyPackages.${final.stdenv.hostPlatform.system}.opencode;
-
-    vimPlugins = prev.vimPlugins // {
-      obsidian-nvim = prev.vimUtils.buildVimPlugin {
-        pname = "obsidian-nvim";
-        version = "2025-11-24";
-        checkInputs = with prev.vimPlugins; [
-          fzf-lua
-          mini-nvim
-          snacks-nvim
-          telescope-nvim
-        ];
-        dependencies = with prev.vimPlugins; [
-          plenary-nvim
-        ];
-        nvimSkipModules = [
-          "minimal"
-        ];
-        src = prev.fetchFromGitHub {
-          owner = "obsidian-nvim";
-          repo = "obsidian.nvim";
-          rev = "4065c233f618cdbfbf084d87e97317f8d019ef59";
-          hash = "sha256-hG1Dji1Xrl7yLLB5nN7twCuldIZt6GYWwJJaYCcGTqc=";
-        };
-      };
-    };
   };
 }
